2a2ca533936b1194609d0709c97aa458dca7877b
jonathan robinson <jonbrobinson@gmail.com>
2014-05-30 11:51:50 -0500

    Demonstrated basic functionality of PHP with HTML

--- public/TODOlist.php
+++ public/TODOlist.php
@@ -7,28 +7,63 @@
 ?>
 
 	<header>
-		<title>Todo_List</title>
+		<title>TODO List</title>
 	</header>
 	<body>
-			<h4>TODO List</h4>
-			<p>
-				<ol>
-					<li>Complete Codeup</li>
-					<li>Present a Project</li>
-					<li>Interview with companies</li>
-				</ol>
-			</p>
-		<form method="POST">
+			<?php
+			define('FILENAME', 'list.txt');
+
+			function load_file($filename){
+				$handle = fopen($filename,'r');
+				$contents = trim(fread($handle,filesize($filename)));
+				$contents_array = explode("\n", $contents);
+				fclose($handle);
+				return $contents_array;
+			}
+
+			function save_file($array,$filename){
+				$handle = fopen($filename,'w');
+				$string = implode("\n", $array);
+				$contents = fwrite($handle,$string);
+				fclose($handle);
+			}
+
+			$items = load_file(FILENAME);
+			if(!empty($_POST)){
+				$push_item = $_POST['item'];
+				array_push($items,$push_item);
+				save_file($items,FILENAME);
+			}
+
+			if(!empty($_GET)){
+				if(($_GET['action'] == 'remove')){
+					unset($items[$_GET['index']]);
+				} else if($_GET['action'] == 'mark'){
+					echo "<h2>{$items[$_GET['index']]} is marked complete</h2>";
+				}
+				save_file($items,FILENAME);
+			}
+
+			?>
+			<ol>
+				<?php
+					foreach($items as $key => $item){
+						echo "<li>{$item}
+								<a href=\"TODOlist.php?action=remove&index={$key}\">Remove Item</a>
+								<a href=\"TODOlist.php?action=mark&index={$key}\">Mark Complete</a>
+							  </li>";
+					}
+				?>
+			</ol>
+		<form method="post" action="TODOlist.php">
 			<p>
 	        <label for="item">Todo Item</label>
 	        <input id="item" name="item" type="text" placeholder="Enter Item">
+
 		    </p>
+
 		    <p>
-		        <label for="password">Password</label>
-		        <input id="password" name="password" type="password" placeholder="Username">
-		    </p>
-		    <p>
-		        <button type="submit">Add Item</button>
+		        <button type="submit">Enter Item</button>
 		    </p>
 		</form>
 	</body>
