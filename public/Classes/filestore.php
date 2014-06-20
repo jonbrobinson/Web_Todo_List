6c0cf2b5d833268b0b4b0ff66de8c77d8c7d676b
jonathan robinson <jonbrobinson@gmail.com>
2014-06-20 10:59:41 -0500

    Added Private and Public Method functionality

--- public/Classes/filestore.php
+++ public/Classes/filestore.php
@@ -2,60 +2,93 @@
 
 class Filestore {
 
+    public $is_csv = false;
+
     public $filename = '';
 
-    function __construct($filename)
+    public function __construct($filename)
     {
         $this->filename = $filename;
+        $extension = substr($filename,-3);
+        if($extension == 'csv'){
+            $this->is_csv = true;
+        }
+
     }
 
-    function read_lines()
+    private function read_lines()
     {
-		$handle = fopen($this->filename,'r');
-		$contents = trim(fread($handle,filesize($this->filename)));
-		$contents_array = explode("\n", $contents);
-		fclose($handle);
-		return $contents_array;
+        $contents_array = [];
+        if(filesize($this->filename) > 0){
+            $handle = fopen($this->filename,'r');
+            $contents = trim(fread($handle,filesize($this->filename)));
+            $contents_array = explode("\n", $contents);
+            fclose($handle);
+        }
+        return $contents_array;
     }
 
     /**
      * Writes each element in $array to a new line in $this->filename
      */
-    function write_lines($array)
+    private  function write_lines($array)
     {
-    	$handle = fopen($this->filename,'w');
-		$string = implode("\n", $array);
-		$contents = fwrite($handle,$string);
-		fclose($handle);
+        $handle = fopen($this->filename,'w');
+        $string = implode("\n", $array);
+        $contents = fwrite($handle,$string);
+        fclose($handle);
     }
 
     /**
      * Reads contents of csv $this->filename, returns an array
      */
-    function read_csv()
+    private  function read_csv()
     {
-    	$address_book = [];
-		$handle = fopen($this->filename, 'r');
-		while(!feof($handle)){
-			$row = fgetcsv($handle);
-			if(is_array($row)){
-				$address_book[] = $row;
-			}
-		}
-		fclose($handle);
-		return $address_book;
+        $address_book = [];
+        $handle = fopen($this->filename, 'r');
+        while(!feof($handle)){
+            $row = fgetcsv($handle);
+            if(is_array($row)){
+                $address_book[] = $row;
+            }
+        }
+        fclose($handle);
+        return $address_book;
     }
 
     /**
      * Writes contents of $array to csv $this->filename
      */
-    function write_csv($array)
+    private  function write_csv($array)
     {
-    	$handle = fopen($this->filename, 'w');
-		foreach ($array as $fields){
-			fputcsv($handle,$fields);
-		}
-		fclose($handle);
+        $handle = fopen($this->filename, 'w');
+        foreach ($array as $fields){
+            fputcsv($handle,$fields);
+        }
+        fclose($handle);
     }
 
+    public  function __destruct(){
+            echo  "Class dismissed";
+    }
+
+    // Create 2 new methods in Filestore: read() and write($array), and make them both public. read() should call $this->read_csv() if $this->is_csv is TRUE, and $this->read_lines() if it is not. write() should work the same way. Now when we can simply set a file, and call read() or write() and get back an array based on the filetype.
+
+    public function read()
+    {
+        if ($this->is_csv) {
+            return $this->read_csv();
+        } else {
+             return $this->read_lines();
+        }
+    }
+
+    public function write($array)
+    {
+        if ($this->is_csv == false) {
+            $this->write_lines($array);
+        } else {
+            $this->write_csv($array);
+        }
+    }
 }
